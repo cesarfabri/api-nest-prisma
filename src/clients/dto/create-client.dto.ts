c1fd5b7e5b15729c6076c08862c556c27079d1d7
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-25 12:03:31 -0300

    Client dto

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,11 @@
-export class CreateClientDto {}
+import { Client } from '../entities/client.entity';
+
+export class CreateClientDto extends Client {
+  name: string;
+  address: string;
+  number: string;
+  district: string;
+  city: string;
+  state: string;
+  zipcode: string;
+}
