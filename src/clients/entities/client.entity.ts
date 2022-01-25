b8c090a0756158b077107c5669094bdcb2791112
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-25 12:02:53 -0300

    Created client.entity and its dto

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,1 +1,17 @@
-export class Client {}
+import { Prisma } from '@prisma/client';
+
+export class Client implements Prisma.ClientsUncheckedCreateInput {
+  id?: string;
+  name: string;
+  address: string;
+  number: string;
+  city: string;
+  district: string;
+  state: string;
+  zipcode: string;
+  create_at?: string | Date;
+  update_at?: string | Date;
+  Contacts?: Prisma.ContactsUncheckedCreateNestedManyWithoutClientInput;
+  Products?: Prisma.ProductsUncheckedCreateNestedManyWithoutClientInput;
+  Orders?: Prisma.OrdersUncheckedCreateNestedManyWithoutClientInput;
+}
