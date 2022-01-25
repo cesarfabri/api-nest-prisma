5ede46f1cbd5397d6e45b5b81e894c5474ebc72c
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-25 11:56:06 -0300

    Resource clients initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
