c0a9485dfc9852c4fd88035c66bae909f51d94ea
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-25 14:02:06 -0300

    Refactored clients.service

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 
 @Injectable()
 export class ClientsService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+    return this.prisma.clients.create({
+      data: createClientDto,
+    });
   }
 
   findAll() {
-    return `This action returns all clients`;
+    return this.prisma.clients.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  findOne(id: string) {
+    return this.prisma.clients.findFirst({
+      where: { id },
+    });
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  update(id: string, updateClientDto: UpdateClientDto) {
+    updateClientDto.update_at = new Date();
+
+    return this.prisma.clients.update({
+      where: { id },
+      data: updateClientDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  remove(id: string) {
+    return this.prisma.clients.delete({
+      where: { id },
+    });
   }
 }
