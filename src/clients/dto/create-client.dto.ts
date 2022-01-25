c80560e8ef170f767c66141f3f7330b1e7dc74c3
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-25 14:40:10 -0300

    Inserted class-validator and class-transformer

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,11 +1,19 @@
+import { IsString } from 'class-validator';
 import { Client } from '../entities/client.entity';
 
 export class CreateClientDto extends Client {
+  @IsString()
   name: string;
+  @IsString()
   address: string;
+  @IsString()
   number: string;
+  @IsString()
   district: string;
+  @IsString()
   city: string;
+  @IsString()
   state: string;
+  @IsString()
   zipcode: string;
 }
